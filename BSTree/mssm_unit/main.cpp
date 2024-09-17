83bf33b074835ea3ad8078d4e3d8e6b3140319c7
sagesanto <chellster1003@gmail.com>
2024-09-17 14:06:39 -0400

    Should be fixed i think i hope

--- BSTree/mssm_unit/main.cpp
+++ BSTree/mssm_unit/main.cpp
@@ -512,6 +512,29 @@ TEST(TestBSTree, HeightTest)
     ASSERT_EQ(tree.getHeight(), 3);
 }
 
+TEST(TestBSTree, CrashTest)
+{
+    BSTree<int> tree;
+    tree.insert(11);
+    tree.insert(3);
+    tree.insert(44);
+    tree.remove(3);
+    tree.insert(1);
+    tree.insert(5);
+    tree.insert(7);
+    tree.remove(5);
+    tree.insert(8);
+    tree.insert(7);
+    tree.insert(6);
+    tree.insert(5);
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+    tree.insert(4);
+    tree.remove(44);
+    ASSERT_TRUE(true);
+}
+
 TEST(TestBSTree, RemoveTest)
 {
     BSTree<int> tree;
