83f4ebe74e5da59d454b74d4f36d2badbb4d2452
Calvin Akom <calvinakom@gmail.com>
2024-09-30 23:01:23 -0400

    Passes tests

--- PriorityQueue/mssm_unit/main.cpp
+++ PriorityQueue/mssm_unit/main.cpp
@@ -5,14 +5,25 @@ template <typename T>
 class PriorityQueue
 {
 private:
-    T treeArr;
+    // Vars
+    T* treeArr;
     int allMem;
-    int size;
+    int _size;
+
+    //Funcs
+    int parent(int index);
+    int child1(int index);
+    int child2(int index);
+    void parentSwap(int index);
+    void childSwap(int index);
 
 public:
     PriorityQueue();
     void insert(T val);
-    void parentSwap(int index);
+    T remove();
+    bool isEmpty();
+    int size();
+
 };
 
 template <typename T>
@@ -20,16 +31,33 @@ PriorityQueue<T>::PriorityQueue()
 {
     treeArr = new T[1];
     allMem = 1;
-    size = 0;
+    _size = 0;
+}
+
+template <typename T>
+int PriorityQueue<T>::parent(int index)
+{
+    return (index - 1) / 2;
+}
+
+template <typename T>
+int PriorityQueue<T>::child1(int index)
+{
+    return index * 2 + 1;
+}
+
+template <typename T>
+int PriorityQueue<T>::child2(int index)
+{
+    return index * 2 + 2;
 }
 
 template <typename T>
 void PriorityQueue<T>::insert(T val)
 {
-    if(size == allocMem)
+    if(_size == allMem)
     {
-        T tempArr = treeArr;
-        delete [] treeArr;
+        T* tempArr = treeArr;
         treeArr = new T[allMem * 2];
         for(int i = 0; i < allMem; i++)
         {
@@ -38,20 +66,120 @@ void PriorityQueue<T>::insert(T val)
         delete [] tempArr;
         allMem *= 2;
     }
-    treeArr[size] = val;
-    parentSwap(size);
-    size++;
+    treeArr[_size] = val;
+    parentSwap(_size);
+    _size++;
 }
 
 template <typename T>
-void PriorityQueue<T>::parentSwap(T val)
+T PriorityQueue<T>::remove()
 {
-    if()
+    T val = treeArr[0];
+    treeArr[0] = treeArr[_size - 1];
+    childSwap(0);
+    _size--;
+    return val;
 }
 
-TEST(Test, Name)
+template <typename T>
+bool PriorityQueue<T>::isEmpty()
 {
+    return _size == 0;
+}
 
+template <typename T>
+int PriorityQueue<T>::size()
+{
+    return _size;
+}
+
+template <typename T>
+void PriorityQueue<T>::parentSwap(int index)
+{
+    if(treeArr[parent(index)] < treeArr[index])
+    {
+        swap(treeArr[parent(index)],treeArr[index]);
+        if(parent(index) > 0) parentSwap(parent(index));
+    }
+}
+
+template <typename T>
+void PriorityQueue<T>::childSwap(int index)
+{
+    bool hasChild1 = (child1(index) < _size);
+    bool hasChild2 = (child2(index) < _size);
+    if(hasChild1 && hasChild2)
+    {
+        T child1Val = treeArr[child1(index)];
+        T child2Val = treeArr[child2(index)];
+        if(child1Val > child2Val && child1Val > treeArr[index]) swap(treeArr[child1(index)],treeArr[index]);
+        else if(child2Val > treeArr[index]) swap(treeArr[child2(index)],treeArr[index]);
+    }
+    else if(hasChild1)
+    {
+        T child1Val = treeArr[child1(index)];
+        if(child1Val > treeArr[index]) swap(treeArr[child1(index)], treeArr[index]);
+    }
+    else if(hasChild2)
+    {
+        T child2Val = treeArr[child2(index)];
+        if(child2Val > treeArr[index]) swap(treeArr[child2(index)], treeArr[index]);
+    }
+}
+
+TEST(TestPQ, InsertTest)
+{
+    PriorityQueue<int> test;
+    test.insert(1);
+    test.insert(2);
+    test.insert(0);
+}
+
+TEST(TestPQ, RemoveTest)
+{
+    PriorityQueue<int> test;
+    test.insert(1);
+    test.insert(2);
+    test.insert(0);
+    vector<int> list = {};
+    for(int i = 0; i < 3; i++)
+    {
+        list.push_back(test.remove());
+    }
+    vector<int> expected = {2,1,0};
+    ASSERT_EQ(list,expected);
+}
+
+TEST(TestPQ, EmptyTest)
+{
+    PriorityQueue<int> test;
+    ASSERT_TRUE(test.isEmpty());
+    test.insert(1);
+    test.insert(2);
+    test.insert(0);
+    for(int i = 0; i < 2; i++)
+    {
+        test.remove();
+    }
+    ASSERT_FALSE(test.isEmpty());
+    test.remove();
+    ASSERT_TRUE(test.isEmpty());
+}
+
+TEST(TestPQ, SizeTest)
+{
+    PriorityQueue<int> test;
+    ASSERT_EQ(test.size(), 0);
+    test.insert(1);
+    test.insert(2);
+    test.insert(0);
+    for(int i = 0; i < 2; i++)
+    {
+        test.remove();
+    }
+    ASSERT_EQ(test.size(), 1);
+    test.remove();
+    ASSERT_EQ(test.size(), 0);
 }
 
 int main()
